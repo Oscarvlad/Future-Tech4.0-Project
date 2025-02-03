a5ca96cf276016e9dae501e3ad73b29ac531b8c3
Oscarvlad <emmanuel001j@gmail.com>
2025-02-03 23:32:19 +0100

    .env file updated

--- FutureTech Backend/server.js
+++ FutureTech Backend/server.js
@@ -18,84 +18,83 @@ app.get("/", (req, res) => {
 
 //order a new Product
 app.post('/api/products', async (req, res) => {
-	 try{ 	
+	try {
 		const product = await Product.create(req.body)
 		res.status(200).json(product);
-	} 
-	catch(error){
-		res.status(500).json({message:"error placing your order", error})
 	}
-	
+	catch (error) {
+		res.status(500).json({ message: "error placing your order", error })
+	}
+
 })
 
 //Get Products
 app.get('/api/products', async (req, res) => {
-	try{
+	try {
 		const products = await Product.find({});
 		res.status(200).json(products);
 
-	}catch(error){
-		res.status(500).json({message:error.message})
+	} catch (error) {
+		res.status(500).json({ message: error.message })
 	}
 });
 
 //Get a single product
 app.get('/api/products:id', async (req, res) => {
-	try{
-	
+	try {
+
 		const product = await Product.findById(id.req.params)
 		res.send(200).json(product)
-	}catch(error){
-		res.status(500).json({message:'error fetching product', error})
+	} catch (error) {
+		res.status(500).json({ message: 'error fetching product', error })
 	}
 })
 
 //update a product
 app.put('/api/products:id', async (req, res) => {
-	try{
-		const {id} = req.params;
+	try {
+		const { id } = req.params;
 
 		const product = await Product.findByIdAndUpdate(id, req.body);
 
-		if(!product){
-			return res.status(404).json({message:'Product not found'})
+		if (!product) {
+			return res.status(404).json({ message: 'Product not found' })
 		}
 
 		const updatedProduct = await Product.findById(id)
 		res.status(200).json(updatedProduct)
 
-	} catch(error){
-		res.status(500).json({message:'error updating product', error})
+	} catch (error) {
+		res.status(500).json({ message: 'error updating product', error })
 	}
 })
 
 //Delete a product
 app.delete('/api/products:id', async (req, res) => {
-	try{
-		const {id} = req.params
+	try {
+		const { id } = req.params
 		const product = await Product.findByIdAndDelete(id)
-		if(!product){
-			res.status(404).json({message: 'error deleting product'})
+		if (!product) {
+			res.status(404).json({ message: 'error deleting product' })
 		}
 
 		deletedProduct = await Product.findById(id)
 		res.status(200).json(deletedProduct)
-			res.status(200).json(deletedProduct)
-		//	res.status(200).json({'product deleted successfully!!})
+		res.status(200).json(deletedProduct)
 
-	}catch(error){
-		res.status(500)({message:'error deleting order', error})
+	} catch (error) {
+		res.status(500)({ message: 'error deleting order', error })
 	}
 })
 
 
 //mongodb connection
-		console.log("connected to mongodb");
-		app.listen(port, () => {
-			console.log(`server running on ${port}`);
-		});
+console.log("connected to mongodb");
+app.listen(port, () => {
+	console.log(`server running on ${port}`);
+});
 	})
 
-	.catch((error) => {
-		console.error("Error connecting to MongoDB:", error);
-	});
+	.catch ((error) => {
+	console.error("Error connecting to MongoDB:", error);
+});
